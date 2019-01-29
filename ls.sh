bf6eb7c421f403e122867e88b7468a3df17ad04a
Emmanuel Frecon <efrecon@gmail.com>
2019-01-29 23:29:04 +0100

    Trying to fix termination catch, WIP

--- ls.sh
+++ ls.sh
@@ -4,13 +4,20 @@ PERIOD=${1:-60}
 DEST=${AWS_S3_MOUNT:-/mnt/bucket}
 
 exit_script() {
-    echo "Unmounting ${DEST}"
+    SIGNAL=$1
+    echo "Caught $SIGNAL! Unmounting ${DEST}..."
     fusermount -uz ${DEST}
-    trap - SIGINT SIGTERM # clear the trap
+    s3fs=$(ps -o pid= -o comm= | grep s3fs | sed -E 's/\s*(\d+)\s+.*/\1/g')
+    if [ -n "$s3fs" ]; then
+        echo "Forwarding $SIGNAL to $s3fs"
+        kill -$SIGNAL $s3fs
+    fi
+    trap - $SIGNAL # clear the trap
     exit $?
 }
 
-trap exit_script SIGINT SIGTERM
+trap "exit_script INT" INT
+trap "exit_script TERM" TERM
 
 while true; do
     ls $DEST
