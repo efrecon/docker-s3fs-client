ac18d62d5954c3eba346efeb8319e663a3347b41
Emmanuel Frécon <efrecon@gmail.com>
2022-03-13 22:20:13 +0100

    Stricter coding (shellcheck)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,13 @@
-#! /usr/bin/env sh
+#!/bin/sh
 
 # Where are we going to mount the remote bucket resource in our container.
 DEST=${AWS_S3_MOUNT:-/opt/s3fs/bucket}
 
 # Check variables and defaults
-if [ -z "${AWS_S3_ACCESS_KEY_ID}" -a -z "${AWS_S3_SECRET_ACCESS_KEY}" -a -z "${AWS_S3_SECRET_ACCESS_KEY_FILE}" -a -z "${AWS_S3_AUTHFILE}" ]; then
+if [ -z "${AWS_S3_ACCESS_KEY_ID}" ] && \
+    [ -z "${AWS_S3_SECRET_ACCESS_KEY}" ] && \
+    [ -z "${AWS_S3_SECRET_ACCESS_KEY_FILE}" ] && \
+    [ -z "${AWS_S3_AUTHFILE}" ]; then
     echo "You need to provide some credentials!!"
     exit
 fi
@@ -17,13 +20,14 @@ if [ -z "${AWS_S3_URL}" ]; then
 fi
 
 if [ -n "${AWS_S3_SECRET_ACCESS_KEY_FILE}" ]; then
-    AWS_S3_SECRET_ACCESS_KEY=$(read ${AWS_S3_SECRET_ACCESS_KEY_FILE})
+    # shellcheck disable=SC2229   # We WANT to read the content of the file pointed by the variable!
+    AWS_S3_SECRET_ACCESS_KEY=$(read -r "${AWS_S3_SECRET_ACCESS_KEY_FILE}")
 fi
 
 # Create or use authorisation file
 if [ -z "${AWS_S3_AUTHFILE}" ]; then
     AWS_S3_AUTHFILE=/opt/s3fs/passwd-s3fs
-    echo "${AWS_S3_ACCESS_KEY_ID}:${AWS_S3_SECRET_ACCESS_KEY}" > ${AWS_S3_AUTHFILE}
+    echo "${AWS_S3_ACCESS_KEY_ID}:${AWS_S3_SECRET_ACCESS_KEY}" > "${AWS_S3_AUTHFILE}"
     chmod 600 ${AWS_S3_AUTHFILE}
 fi
 
@@ -34,30 +38,28 @@ if [ -n "${AWS_S3_SECRET_ACCESS_KEY}" ]; then
 fi
 
 # Create destination directory if it does not exist.
-if [ ! -d $DEST ]; then
-    mkdir -p $DEST
+if [ ! -d "$DEST" ]; then
+    mkdir -p "$DEST"
 fi
 
 GROUP_NAME=$(getent group "${GID}" | cut -d":" -f1)
 
 # Add a group
-if [ $GID -gt 0 -a -z "${GROUP_NAME}" ]; then
-    addgroup -g $GID -S $GID
+if [ "$GID" -gt 0 ] && [ -z "${GROUP_NAME}" ]; then
+    addgroup -g "$GID" -S "$GID"
     GROUP_NAME=$GID
 fi
 
 # Add a user
-if [ $UID -gt 0 ]; then
-    adduser -u $UID -D -G $GROUP_NAME $UID
+if [ "$UID" -gt 0 ]; then
+    adduser -u "$UID" -D -G "$GROUP_NAME" "$UID"
     RUN_AS=$UID
-    chown $UID:$GID $AWS_S3_MOUNT
-    chown $UID:$GID ${AWS_S3_AUTHFILE}
-    chown $UID:$GID /opt/s3fs
+    chown "$UID:$GID" "$AWS_S3_MOUNT" "${AWS_S3_AUTHFILE}" /opt/s3fs
 fi
 
 # Debug options
 DEBUG_OPTS=
-if [ $S3FS_DEBUG = "1" ]; then
+if [ "$S3FS_DEBUG" = "1" ]; then
     DEBUG_OPTS="-d -d"
 fi
 
@@ -81,9 +83,8 @@ su - $RUN_AS -c "s3fs $DEBUG_OPTS ${S3FS_ARGS} \
 # Doing an operation actually forces it to detect that and remove the mount.
 ls "${AWS_S3_MOUNT}"
 
-mounted=$(mount | grep fuse.s3fs | grep "${AWS_S3_MOUNT}")
-if [ -n "${mounted}" ]; then
-    echo "Mounted bucket ${AWS_S3_BUCKET} onto ${AWS_S3_MOUNT}"
+if healthcheck.sh; then
+    echo "Mounted bucket ${AWS_S3_BUCKET} onto ${DEST}"
     exec "$@"
 else
     echo "Mount failure"
--- trap.sh
+++ trap.sh
@@ -1,13 +1,16 @@
+#!/bin/sh
+
 exit_script() {
     SIGNAL=$1
     echo "Caught $SIGNAL! Unmounting ${DEST}..."
-    fusermount -uz ${DEST}
+    fusermount -uz "${DEST}"
+    # shellcheck disable=SC2009
     s3fs=$(ps -o pid= -o comm= | grep s3fs | sed -E 's/\s*(\d+)\s+.*/\1/g')
     if [ -n "$s3fs" ]; then
         echo "Forwarding $SIGNAL to $s3fs"
-        kill -$SIGNAL $s3fs
+        kill -"$SIGNAL" "$s3fs"
     fi
-    trap - $SIGNAL # clear the trap
+    trap - "$SIGNAL" # clear the trap
     exit $?
 }
 
